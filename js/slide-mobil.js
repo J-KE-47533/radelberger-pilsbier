29eede0d7f0213a5bb98be2da26a42d046d82350
J-KE-47533 <82226827+J-KE-47533@users.noreply.github.com>
2024-06-20 17:54:50 +0000

    update vom lokalen Speicher

--- js/slide-mobil.js
+++ js/slide-mobil.js
@@ -1,23 +1,3 @@
-let currentIndex = 0;
-const slides = document.querySelectorAll('.slide');
-const totalSlides = slides.length;
-
-function showSlide(index) {
-    slides.forEach((slide, i) => {
-        slide.classList.toggle('active', i === index);
-    });
-}
-
-showSlide(currentIndex)
-
-function nextSlide() {
-    currentIndex = (currentIndex + 1) % totalSlides;
-    showSlide(currentIndex);
-}
-
-setInterval(nextSlide, 3000); // Wechsel alle 3 Sekunden
-
-// Variablen für Touch-Ereignisse
 let startX, startY, endX, endY;
 
 function handleTouchStart(event) {
